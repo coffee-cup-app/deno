0ac55ea921df070e0e982a331185e674545ccb26
Jake Runzer <jakerunzer@gmail.com>
2023-04-07 23:12:20 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts"
 
 const port = parseInt(Deno.env.get("PORT") ?? "8000");
-serve(() => new Response("Choo Choo! Welcome to your Deno app 22222\n"),
+serve(() => new Response("Choo Choo! Welcome to your Deno app shdjfjf22\n"),
       { addr: `:${port}` });
 
 console.log(`http://localhost:${port}/`);
